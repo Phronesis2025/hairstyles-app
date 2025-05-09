5b7f53634d45b55463259a8746323cdbe6b5c3cb
Jason Boyer <phronesis700@gmail.com>
2025-05-09 08:39:08 -0500

    Update vercel.json

---
+++ client/src/test.jsx
@@ -0,0 +1,5 @@
+function TempComponent() {
+  return <div>Test component</div>;
+}
+
+export default TempComponent;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/health", (req, res) => {
-  res.json({ status: "Server updated" });
+  res.json({ status: "Server running" });
 });
 
 app.listen(port, () => {
